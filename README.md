cd14f55027e8b319e4a568e3b0e1d89a72c36441
Lucer9 <carlos.aguirreorozco@gmail.com>
2022-01-17 15:33:44 -0600

    Small improvements

--- README.md
+++ README.md
@@ -1,27 +1,24 @@
-# AngularTests
+# Simple Angular Search Listing
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.5.
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.5.  
+Used [TMDB API ](https://www.themoviedb.org) for the mock data, and even though the API comes with searching endpoints, it was all done in the frontend.
+All the CSS was handmade from a personal design    
+A demo can be found [here](https://peaceful-kowalevski-a35e88.netlify.app/)    
+
+**Note**
+>The demo has to get local data, because the API request is on HTTP while the site is on HTTPS
+
 
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
-## Code scaffolding
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
 
 ## Build
 
 Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
 
-## Running unit tests
+Made by Carlos Aguirre from INOWU
 
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { MoviesService } from "./services/movies.service";
+import { movies } from "./components/movies";
 
 @Component({
   selector: "app-root",
@@ -8,6 +9,7 @@ import { MoviesService } from "./services/movies.service";
 })
 export class AppComponent implements OnInit {
   title = "Popcorn 2.0";
+  
   movies = [];
   backupMovies = [];
   loading = true;
@@ -15,16 +17,27 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.loading = true;
-    this.moviesService.getContent(1).subscribe((page1: any) => {
-      this.moviesService.getContent(2).subscribe((page2: any) => {
-        this.moviesService.getContent(3).subscribe((page3: any) => {
-          setTimeout(() => {
-            this.movies = page1.results.concat(page2.results, page3.results);
-            this.backupMovies = this.movies
-            this.loading = false;
-          }, 500);
+    this.moviesService.getContent(1).subscribe(
+      (page1: any) => {
+        this.moviesService.getContent(2).subscribe((page2: any) => {
+          this.moviesService.getContent(3).subscribe((page3: any) => {
+            setTimeout(() => {
+              this.movies = page1.results.concat(page2.results, page3.results);
+              this.backupMovies = this.movies;
+              this.loading = false;
+            }, 500);
+          });
         });
-      });
-    });
+      },
+      (error) => {
+        alert("Error fetching data from API. Local data will be loaded")
+        setTimeout(() => {
+          this.movies = movies
+          this.backupMovies = this.movies;
+          this.loading = false;
+        }, 500);
+
+      }
+    );
   }
 }
